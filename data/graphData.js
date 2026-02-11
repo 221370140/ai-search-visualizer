26f66ed5573ef8ca64fa8924ba116927088d0180
221370140 <221370140@gift.edu.pk>
2026-02-11 10:34:13 +0500

    Update graphData.js

--- data/graphData.js
+++ data/graphData.js
@@ -1,30 +1,30 @@
 
 
-export const cities = [
-  { id: "Karachi",      lat: 24.8607, lon: 67.0011 },
-  { id: "Hyderabad",    lat: 25.3960, lon: 68.3578 },
-  { id: "Sukkur",       lat: 27.7052, lon: 68.8574 },
-  { id: "Quetta",       lat: 30.1798, lon: 66.9750 },
+const cities = [
+  { id: "Karachi", lat: 24.8607, lon: 67.0011 },
+  { id: "Hyderabad", lat: 25.3960, lon: 68.3578 },
+  { id: "Sukkur", lat: 27.7052, lon: 68.8574 },
+  { id: "Quetta", lat: 30.1798, lon: 66.9750 },
 
-  { id: "Multan",       lat: 30.1575, lon: 71.5249 },
-  { id: "Bahawalpur",   lat: 29.3544, lon: 71.6911 },
-  { id: "Lahore",       lat: 31.5204, lon: 74.3587 },
-  { id: "Faisalabad",   lat: 31.4180, lon: 73.0790 },
+  { id: "Multan", lat: 30.1575, lon: 71.5249 },
+  { id: "Bahawalpur", lat: 29.3544, lon: 71.6911 },
+  { id: "Lahore", lat: 31.5204, lon: 74.3587 },
+  { id: "Faisalabad", lat: 31.4180, lon: 73.0790 },
 
-  { id: "Gujranwala",   lat: 32.1877, lon: 74.1945 },
-  { id: "Gujrat",       lat: 32.5731, lon: 74.0750 },
-  { id: "Sialkot",      lat: 32.4945, lon: 74.5229 },
-  { id: "Jhelum",       lat: 32.9331, lon: 73.7264 },
-  { id: "Sargodha",     lat: 32.0836, lon: 72.6711 },
+  { id: "Gujranwala", lat: 32.1877, lon: 74.1945 },
+  { id: "Gujrat", lat: 32.5731, lon: 74.0750 },
+  { id: "Sialkot", lat: 32.4945, lon: 74.5229 },
+  { id: "Jhelum", lat: 32.9331, lon: 73.7264 },
+  { id: "Sargodha", lat: 32.0836, lon: 72.6711 },
 
-  { id: "Islamabad",    lat: 33.6844, lon: 73.0479 },
-  { id: "Rawalpindi",   lat: 33.5651, lon: 73.0169 },
-  { id: "Peshawar",     lat: 34.0151, lon: 71.5249 },
-  { id: "Mardan",       lat: 34.1986, lon: 72.0404 },
-  { id: "Abbottabad",   lat: 34.1688, lon: 73.2215 },
+  { id: "Islamabad", lat: 33.6844, lon: 73.0479 },
+  { id: "Rawalpindi", lat: 33.5651, lon: 73.0169 },
+  { id: "Peshawar", lat: 34.0151, lon: 71.5249 },
+  { id: "Mardan", lat: 34.1986, lon: 72.0404 },
+  { id: "Abbottabad", lat: 34.1688, lon: 73.2215 },
 
   { id: "Muzaffarabad", lat: 34.3700, lon: 73.4700 },
-  { id: "Gilgit",       lat: 35.9208, lon: 74.3080 },
+  { id: "Gilgit", lat: 35.9208, lon: 74.3080 },
 ];
 
 
@@ -50,7 +50,7 @@ function haversineDistanceKm(a, b) {
 
 
 
-export function projectCitiesToSvg() {
+function projectCitiesToSvg() {
   const lats = cities.map(c => c.lat);
   const lons = cities.map(c => c.lon);
 
@@ -129,7 +129,7 @@ const roads = [
 
 
 
-export function buildGraph() {
+function buildGraph() {
   const cityById = Object.fromEntries(
     cities.map(c => [c.id, c])
   );
